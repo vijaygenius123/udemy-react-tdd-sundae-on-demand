5122643adc04dc87d107f0af79bc738832b6f933
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-12-30 13:05:36 +0530

    Replace fireEvent with userEvent

--- packages/frontend-app/src/components/summary/test/SummaryForm.test.jsx
+++ packages/frontend-app/src/components/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { screen, render, fireEvent } from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import { SummaryForm } from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 describe("Summary Form", function () {
   it("should have initial state as unchecked checkbox and disabled button", function () {
@@ -12,15 +13,16 @@ describe("Summary Form", function () {
     expect(button).toBeDisabled();
   });
 
-  it("should enable button when checkbox is checked", function () {
+  it("should enable button when checkbox is checked", async function () {
+    const user = userEvent.setup();
     render(<SummaryForm />);
     const button = screen.getByRole("button", { name: /confirm order/i });
     const checkbox = screen.getByRole("checkbox", {
       name: /i agree to terms and conditions/i,
     });
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(button).toBeEnabled();
-    fireEvent.click(checkbox);
+    await user.click(checkbox);
     expect(button).toBeDisabled();
   });
 });
