ef8d5bdfbaf7afbabc5b4cffa49ddd72c5e8caac
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2023-01-01 00:11:49 +0530

    MSW setup

--- packages/frontend-app/src/mocks/handlers.js
+++ packages/frontend-app/src/mocks/handlers.js
@@ -1,1 +1,12 @@
-//import { rest } from "msw";
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("http://localhost:8080/scoops", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Chocolate", image: "/images/chocolate.png" },
+        { name: "Vanilla", image: "/images/vanilla.png" },
+      ])
+    );
+  }),
+];
